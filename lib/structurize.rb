77f9cf6850171959ba0a2d049d8c73e11e76e233
Balaji <balaji2682@gmail.com>
2023-02-12 21:25:57 +0530

    Project initalization

---
+++ lib/structurize.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "structurize/version"
+
+module Structurize
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/structurize_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Structurize do
+  it "has a version number" do
+    expect(Structurize::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
